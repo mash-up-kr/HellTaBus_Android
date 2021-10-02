75103d1a231bc68fe38d92dc159e6332de2aa519
Leo <lee2928030@naver.com>
2021-10-02 21:53:50 +0900

    feat: event wrapper class 추가

---
+++ presentation/src/main/java/com/mashup/presentation/base/Event.kt
@@ -0,0 +1,27 @@
+/*
+ * Created by Leo on 2021. 10. 02 ..
+ */
+package com.mashup.presentation.base
+
+import androidx.lifecycle.Observer
+
+open class Event<out T>(private val content: T) {
+    private var hasBeenHandled = false
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
+
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let { value -> onEventUnhandledContent(value) }
+    }
+}
