0bff13046814a3b749b27a43f4b5f5f51ee834d4
d2fault <d2fault@naver.com>
2021-11-08 01:47:10 +0900

    🧸feat: 운동 요약 페이지 레이아웃을 추가합니다.

---
+++ app/src/main/java/com/mashup/healthyup/features/ExerciseSummaryActivity.kt
@@ -0,0 +1,12 @@
+package com.mashup.healthyup.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mashup.healthyup.R
+
+class ExerciseSummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_summary)
+    }
+}
