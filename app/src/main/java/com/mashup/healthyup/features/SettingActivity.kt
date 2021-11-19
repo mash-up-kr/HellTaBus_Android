211ef29b375c581be2b1d3fd740e3be0de7bfc98
d2fault <d2fault@naver.com>
2021-11-19 10:31:00 +0900

    🧸feat: SettingActivity를 추가합니다.

---
+++ app/src/main/java/com/mashup/healthyup/features/SettingActivity.kt
@@ -0,0 +1,15 @@
+package com.mashup.healthyup.features
+
+import android.os.Bundle
+import com.mashup.healthyup.R
+import com.mashup.healthyup.base.BaseActivity
+import com.mashup.healthyup.databinding.ActivitySettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
