72ae4dcfc9c40266b27925af9c116d6abbb01ab5
Leo <lee2928030@naver.com>
2021-11-20 16:33:24 +0900

    운동중 하단 progressbar를 나타내기 위해 layout을 추가하고, linearLayout addview 추가

--- app/src/main/java/com/mashup/healthyup/features/LauncherActivity.kt
+++ app/src/main/java/com/mashup/healthyup/features/LauncherActivity.kt
@@ -2,13 +2,12 @@ package com.mashup.healthyup.features
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.mashup.healthyup.R
 import com.mashup.healthyup.base.BaseActivity
 import com.mashup.healthyup.bridge.JavaScriptInterface
 import com.mashup.healthyup.databinding.ActivityLauncherBinding
+import com.mashup.healthyup.features.exercise.ExerciseDashboardFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LauncherActivity : BaseActivity<ActivityLauncherBinding>(R.layout.activity_launcher) {
@@ -18,9 +17,14 @@ class LauncherActivity : BaseActivity<ActivityLauncherBinding>(R.layout.activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch {
-            javaScriptInterface = binding.healthyUpWebView.javaScriptInterface
-            binding.healthyUpWebView.loadUrl("https://helltabus-dev.netlify.app/survey")
-        }
+//        lifecycleScope.launch {
+//            javaScriptInterface = binding.healthyUpWebView.javaScriptInterface
+//            binding.healthyUpWebView.loadUrl("https://helltabus-dev.netlify.app/survey")
+//        }
+
+        val fragment = ExerciseDashboardFragment.newInstance(0L)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment, "dashboard")
+            .commitNow()
     }
 }
--- app/src/main/java/com/mashup/healthyup/features/exercise/ExerciseDashboardFragment.kt
+++ app/src/main/java/com/mashup/healthyup/features/exercise/ExerciseDashboardFragment.kt
@@ -4,13 +4,19 @@
 package com.mashup.healthyup.features.exercise
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.mashup.healthyup.Key
 import com.mashup.healthyup.R
 import com.mashup.healthyup.base.BaseFragment
 import com.mashup.healthyup.databinding.ActivityExerciseDashboardBinding
+import com.mashup.healthyup.databinding.LayoutProgressBinding
 import com.mashup.healthyup.features.LauncherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,10 +35,32 @@ class ExerciseDashboardFragment :
         }
     }
 
+    override fun initViews() {
+        // TODO : layout_progress 동적으로 addView
+        addProgressViews()
+    }
+
+    private fun addProgressViews() {
+        val params = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        params.weight = 1f
+
+        listOf(0, 1, 2).forEach { _ ->
+            val progressView = LayoutProgressBinding.inflate(LayoutInflater.from(context)).root
+            binding.layoutProgress.addView(progressView, params)
+        }
+    }
+
     companion object {
 
-        fun newInstance(): ExerciseDashboardFragment {
-            return ExerciseDashboardFragment()
+        fun newInstance(exerciseId: Long): ExerciseDashboardFragment {
+            return ExerciseDashboardFragment().apply {
+                arguments = bundleOf(
+                    Key.EXERCISE_ID to exerciseId
+                )
+            }
         }
     }
 }
