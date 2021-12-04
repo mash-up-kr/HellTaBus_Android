6e11c35a8bffaee9fd471763e495bff5a24ae163
Leo <lee2928030@naver.com>
2021-12-04 16:03:47 +0900

    로그인 유무 체크를 담당하기 위해서 LauncherViewModel 에 channel 을 추가하고, 시작 화면으로 사용될 수 있도록 변경함.

--- app/src/main/java/com/mashup/healthyup/features/LauncherActivity.kt
+++ app/src/main/java/com/mashup/healthyup/features/LauncherActivity.kt
@@ -8,25 +8,24 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.mashup.healthyup.R
 import com.mashup.healthyup.base.BaseActivity
-import com.mashup.healthyup.bridge.JavaScriptInterface
 import com.mashup.healthyup.databinding.ActivityLauncherBinding
 import com.mashup.healthyup.features.LauncherViewModel.Action.StartLogin
 import com.mashup.healthyup.features.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LauncherActivity : BaseActivity<ActivityLauncherBinding>(R.layout.activity_launcher) {
 
-    private var javaScriptInterface: JavaScriptInterface? = null
     private val viewModel by viewModels<LauncherViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launchWhenStarted {
+        lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.actionFlow.onEach { action ->
+                viewModel.actionFlow.collect { action ->
                     when (action) {
                         StartLogin -> startLoginActivity()
                     }
