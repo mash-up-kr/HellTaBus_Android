6cbecd1b0d6035bc45718cc41e25046e32b7e3e2
d2fault <d2fault@naver.com>
2021-12-08 01:49:42 +0900

    🧸feat: Splash 화면에서 token 유무에 따라 view를 분기합니다.

--- app/src/main/java/com/mashup/healthyup/features/launcher/LauncherActivity.kt
+++ app/src/main/java/com/mashup/healthyup/features/launcher/LauncherActivity.kt
@@ -8,9 +8,13 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.mashup.healthyup.R
 import com.mashup.healthyup.base.BaseActivity
+import com.mashup.healthyup.bridge.HealthyUpWebView
 import com.mashup.healthyup.databinding.ActivityLauncherBinding
+import com.mashup.healthyup.features.launcher.LauncherViewModel.Action.StartHome
 import com.mashup.healthyup.features.launcher.LauncherViewModel.Action.StartLogin
 import com.mashup.healthyup.features.login.LoginActivity
+import com.mashup.healthyup.features.web.HealthyUpWebViewActivity
+import com.mashup.healthyup.features.web.WebConstants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -28,6 +32,7 @@ class LauncherActivity : BaseActivity<ActivityLauncherBinding>(R.layout.activity
                 viewModel.actionFlow.collect { action ->
                     when (action) {
                         StartLogin -> startLoginActivity()
+                        StartHome -> startHomeActivity()
                     }
                 }
             }
@@ -37,6 +42,16 @@ class LauncherActivity : BaseActivity<ActivityLauncherBinding>(R.layout.activity
     private fun startLoginActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+        overridePendingTransition(0, 0);
+        finish()
+    }
+
+    private fun startHomeActivity() {
+        HealthyUpWebViewActivity.start(
+            context = this,
+            loadUrl = WebConstants.URL.HOME
+        )
+        overridePendingTransition(0, 0);
         finish()
     }
 }
--- app/src/main/java/com/mashup/healthyup/features/launcher/LauncherViewModel.kt
+++ app/src/main/java/com/mashup/healthyup/features/launcher/LauncherViewModel.kt
@@ -1,7 +1,9 @@
 package com.mashup.healthyup.features.launcher
 
 import androidx.lifecycle.viewModelScope
+import com.mashup.healthyup.Key
 import com.mashup.healthyup.base.BaseViewModel
+import com.mashup.healthyup.bridge.WebPreference
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
@@ -11,10 +13,13 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LauncherViewModel @Inject constructor() : BaseViewModel() {
+class LauncherViewModel @Inject constructor(
+    private val webPreference: WebPreference
+) : BaseViewModel() {
 
     sealed class Action {
-        object StartLogin : Action()
+        object StartLogin: Action()
+        object StartHome: Action()
     }
 
     private val action = Channel<Action>(Channel.BUFFERED)
@@ -23,10 +28,13 @@ class LauncherViewModel @Inject constructor() : BaseViewModel() {
 
     init {
         // TODO : 로그인 유무 체크, 로그인 되어 있으면, 홈화면으로 이동, 로그인 안되어 있으면, 로그인 화면으로 이동
-
         viewModelScope.launch {
-            delay(200L)
-            action.trySend(Action.StartLogin)
+            delay(1000L)
+            if(webPreference.preference.getString(Key.TOKEN, "") != "") {
+                action.trySend(Action.StartHome)
+            } else {
+                action.trySend(Action.StartLogin)
+            }
         }
     }
 }
