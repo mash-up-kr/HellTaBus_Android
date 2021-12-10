c645397bf946cf89b5bf68aafe4e9b9c3fb2d6c1
d2fault <d2fault@naver.com>
2021-12-11 00:09:48 +0900

    🛠refactor: activity_setting.xml에 FragmentContainerView를 추가합니다.

---
+++ app/src/main/java/com/mashup/healthyup/features/setting/SettingViewModel.kt
@@ -0,0 +1,10 @@
+package com.mashup.healthyup.features.setting
+
+import com.mashup.healthyup.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor() : BaseViewModel() {
+
+}
